45654091453e23168917d1a826aabd492dfbf33a
ashtonhawkins <125323430+ashtonhawkins@users.noreply.github.com>
2026-02-21 13:14:33 -0800

    Pass Nucleus secrets to deploy build

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,15 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly PUBLIC_LETTERBOXD_USERNAME?: string;
-  readonly TRAKT_CLIENT_ID?: string;
-  readonly TRAKT_USERNAME?: string;
-  readonly STRAVA_CLIENT_ID?: string;
-  readonly STRAVA_CLIENT_SECRET?: string;
-  readonly STRAVA_REFRESH_TOKEN?: string;
+  readonly PUBLIC_LASTFM_API_KEY: string;
+  readonly PUBLIC_LASTFM_USERNAME: string;
+  readonly PUBLIC_LETTERBOXD_USERNAME: string;
+  readonly TRAKT_USERNAME: string;
+  readonly TRAKT_CLIENT_ID: string;
+  readonly STRAVA_CLIENT_ID: string;
+  readonly STRAVA_CLIENT_SECRET: string;
+  readonly STRAVA_REFRESH_TOKEN: string;
+  readonly LITERAL_API_TOKEN: string;
 }
 
 interface ImportMeta {
